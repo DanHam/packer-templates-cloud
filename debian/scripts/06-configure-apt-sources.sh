ca2d88af6be4f9de160c111981395409ba3652f3
DanHam <DanHam@users.noreply.github.com>
2018-11-27 12:57:38 +0000

    Add contrib to apt sources. Add note regarding cloud-init overwrite

--- debian/scripts/06-configure-apt-sources.sh
+++ debian/scripts/06-configure-apt-sources.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #
 # Configure APT sources
+#
+# Warning: The sources configured here will be overwritten by a cloud-init
+# enabled instance on first boot unless 'apt_preserve_sources_list: true'
+# is written to /etc/cloud/cloud.cfg or specified in the user-data
 set -o errexit
 
 # Set verbose/quiet output based on env var configured in Packer template
@@ -19,9 +23,9 @@ echo "Creating sources.list file for Debian ${codename}" >${redirect}
 
 sources="/etc/apt/sources.list"
 cat > /target/${sources} << EOF
-deb ${APT_MIRROR}/debian ${codename} main
-deb http://security.debian.org/debian-security ${codename}/updates main
-deb ${APT_MIRROR}/debian ${codename}-updates main
+deb ${APT_MIRROR}/debian ${codename} main contrib
+deb http://security.debian.org/debian-security ${codename}/updates main contrib
+deb ${APT_MIRROR}/debian ${codename}-updates main contrib
 EOF
 
 exit 0
