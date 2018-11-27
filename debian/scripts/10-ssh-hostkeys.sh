253eb4e5588af9c429570170e28d7c5ce545b64e
DanHam <DanHam@users.noreply.github.com>
2018-11-27 10:56:38 +0000

    Update ssh host key script for Packer and instances with cloud-init
    * Set to exit on error or when a variable is unset
    * Only install the systemd unit and script if cloud-init will not be
      installed on the system - by default cloud-init will create ssh keys
      the first time it is run
    * Update to reference the location to which Packer uploads the systemd
      unit file and script

--- debian/scripts/10-ssh-hostkeys.sh
+++ debian/scripts/10-ssh-hostkeys.sh
@@ -2,8 +2,18 @@
 #
 # Remove the target systems ssh host keys. This ensures that the systems
 # created from the build image do not all have the same ssh keys.
-# Create a service to re-create/generate the ssh host keys on system
-# startup prior to start up of the sshd server.
+#
+# Regeneration of the keys will occur automatically for systems with
+# cloud-init installed.
+#
+# For systems without cloud-init we need to create a service to
+# re-create/generate the ssh host keys on system startup prior to start up
+# of the sshd server. Packer should have uploaded the required unit file
+# and script to the location given by the TARGET_FILES_DIR env var. The env
+# var should be set in the Packer template and exported in the provisioner
+# calling this script
+set -o errexit
+set -o nounset
 
 # Set verbose/quiet output based on env var configured in Packer template
 [ "$DEBUG" = true ] && redirect="/dev/stdout" || redirect="/dev/null"
@@ -14,10 +24,14 @@ echo "Ensuring all systems created from target have unique ssh host keys"
 # Remove all ssh host keys
 rm -f /target/etc/ssh/ssh_host_*_key*
 
-# Copy the unit file and associated script into place and ensure the
-# service is set to start on boot
-cp  ../files/generate-ssh-host-keys.service /target/etc/systemd/system/
-cp ../files/generate-ssh-host-keys.sh /target/usr/local/sbin/
+# Exit if cloud-init is going to be installed. Otherwise copy the unit file
+# and associated script into place and ensure the service is set to start
+# on boot
+[ "$ENABLE_CLOUD_INIT" = true ] && exit 0
+
+cp ${TARGET_FILES_DIR}/generate-ssh-host-keys.service \
+    /target/etc/systemd/system/
+cp ${TARGET_FILES_DIR}/generate-ssh-host-keys.sh /target/usr/local/sbin/
 chmod +x /target/usr/local/sbin/generate-ssh-host-keys.sh
 chroot /target systemctl enable generate-ssh-host-keys.service
 
