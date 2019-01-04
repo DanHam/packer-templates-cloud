10522994503dad594e3e68e0c6936000e6ec99e8
DanHam <DanHam@users.noreply.github.com>
2019-01-04 18:37:03 +0000

    CentOS: Enable ENA support flag and build generic initramfs

--- centos/scripts/02-install-system-packages.sh
+++ centos/scripts/02-install-system-packages.sh
@@ -10,12 +10,13 @@ set -o errexit
 echo "Installing all required CentOS packages in the chroot target..."
 
 packages="audit authconfig basesystem bash bash-completion biosdevname \
-coreutils cronie curl deltarpm dhclient filesystem glibc grub2 hostname \
-hwdata initscripts iproute iprutils iptables iputils irqbalance kbd \
-kernel less lshw lsof man-db ncurses openssh-clients openssh-server \
-openssl parted passwd policycoreutils policycoreutils-restorecond \
-procps-ng rootfiles rpm rsyslog selinux-policy-targeted setup shadow-utils \
-sudo systemd tar tree tuned util-linux vim-minimal xfsprogs yum"
+coreutils cronie curl deltarpm dhclient dracut-config-generic filesystem \
+glibc grub2 hostname hwdata initscripts iproute iprutils iptables iputils \
+irqbalance kbd kernel less lshw lsof man-db ncurses openssh-clients \
+openssh-server openssl parted passwd policycoreutils \
+policycoreutils-restorecond procps-ng rootfiles rpm rsyslog \
+selinux-policy-targeted setup shadow-utils sudo systemd tar tree tuned \
+util-linux vim-minimal xfsprogs yum"
 
 chroot /target yum install -y ${packages} >${redirect} 2>&1
 
